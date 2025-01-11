dee032aaaf7976a8a8a5cafa8454640d3f2c0f9a
Vysakh-Poulose <vyshak.poulose@gmail.com>
2025-01-11 16:55:54 +0530

    Added tests for edge cases

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -20,4 +20,24 @@ class StringCalculatorTest < Minitest::Test
     result = @calculator.add("12,")
     assert_equal 12, result
   end
+
+  def test_single_number_along_with_multiple_commas_should_return_the_same_number
+    result = @calculator.add("12,,,,")
+    assert_equal 12, result
+  end
+
+  def test_two_numbers_should_return_their_sum
+    result =  @calculator.add("12,1")
+    assert_equal 13, result
+  end
+
+  def test_multiple_numbers_should_return_their_sum
+    result =  @calculator.add("1,2,3,4")
+    assert_equal 10, result
+  end
+
+  def test_multiple_numbers_with_random_commas_should_return_their_sum
+    result =  @calculator.add("1,,2,,,4")
+    assert_equal 7, result
+  end
 end
